f078145c2f6eaa38896bc56dad02b1694f241112
foyez <foyez@foyezs-MacBook-Pro.local>
2020-05-29 10:45:23 -0400

    first commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,35 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        //variables - little empty buckets
+
+       // String myName ="Abdul";
+        String lastName="Foyez";
+
+     // integers
+        int myAge = 25;
+
+        //Byte -small type
+        byte myByte = 127;
+
+        //short 2x byte
+        short myShort = 128;
+
+        //float 1.34, 3.14,....
+        float piNum = 3.14f;
+        //Double
+        double myDouble= 23.89;
+
+        //char == character(&, $, @., 0..)
+        char myChar = 'h';
+
+        //boolean = true or false, 1 or 0
+        boolean myBool= true;
+        System.out.println(" my myChar is " + myChar + " and my boolean is"+ myBool);
+
+
+        //System.out.println("hi my name is "+lastName + " and i am "+myAge+ " years old.");
+    }
+}
